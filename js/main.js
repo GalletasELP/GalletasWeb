5be6d1fe1af2b27511ec648fa6f3a10af9ae0d89
Andres Yunda <grupo.gralleta4b@gmail.com>
2021-12-07 19:33:21 +0100

    update_1

--- js/main.js
+++ js/main.js
@@ -23,25 +23,28 @@ function myFunction() {
 
 // body info
 
-
+document.getElementById("defaultOpen").click();
 
 function openInfo(evt, title) {
     let i, tabcontent, tablinks;
 
-
     tabcontent = document.getElementsByClassName("tabcontent");
+
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+
     tablinks = document.getElementsByClassName("tablinks");
+
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
     document.getElementById(title).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
+
 
 
 let acc = document.getElementsByClassName("accordion");
