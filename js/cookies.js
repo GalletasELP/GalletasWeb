bfa6fbc1382214423869355aaa69357669ea99fa
GalletasELP <95703018+GalletasELP@users.noreply.github.com>
2021-12-13 20:23:02 +0000

    Deploying to gh-pages from @ GalletasELP/GalletasWeb@082a262e70d95e3a56319cd7766bc7d0796ffe6a 🚀

--- js/cookies.js
+++ js/cookies.js
@@ -14,7 +14,7 @@ let aceptar_cookies = false;
 
 function checkCookieVisita() {
     let visit = (document.cookie.indexOf('visita=') === -1 ? '' : ("; " + document.cookie).split('; visita=')[1].split(';')[0]);
-
+    visit++;
     if (usuario.length === 0 && aceptar_cookies === false) {
         //crearModifCookieUser();
         bienvenido.innerHTML = 'Hola, Bienvenido! ';
@@ -40,7 +40,7 @@ function checkCookieVisita() {
     } else {
         bienvenido.innerHTML = 'Nos alegra verte de nuevo...';
         bienvenido_2.innerHTML = 'Has visitado este sitio ' + visit + ' veces.</b>';
-        visit++;
+        
 
     }
     
